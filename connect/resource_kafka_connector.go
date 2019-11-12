1428c9eb440efdfe27395c746bdc03b4364cd02f
Boris Shilov <boris@dataroots.io>
2019-11-12 21:56:12 +0100

    first sketch

--- connect/resource_kafka_connector.go
+++ connect/resource_kafka_connector.go
@@ -138,12 +138,28 @@ func connectorRead(d *schema.ResourceData, meta interface{}) error {
 
 func configFromRD(d *schema.ResourceData) map[string]string {
 	cfg := d.Get("config").(map[string]interface{})
+	scfg := d.Get("config_sensitive").(map[string]interface{})
 	config := make(map[string]string)
 
-	for k, v := range cfg {
-		switch v := v.(type) {
-		case string:
-			config[k] = v
+	for k1, v1 := range cfg {
+		for k2, v2 := range scfg {
+			// if there is a key conflict keep only the sensitive values
+			if k1 == k2 {
+				switch v2 := v2.(type) {
+				case string:
+					config[k2] = v2
+				}
+			// otherwise insert both into final config
+			} else {
+				switch v1 := v1.(type) {
+				case string:
+					config[k1] = v1
+				}
+				switch v2 := v2.(type) {
+				case string:
+					config[k2] = v2
+				}
+			}
 		}
 	}
 
