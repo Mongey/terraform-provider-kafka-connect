bbd329e33a664cb193da9754bd501a62882198b4
Herve <herve.esir@gmail.com>
2022-03-10 17:37:52 +0100

    Add provider support for InsecureSSL or support client certificates

--- connect/provider.go
+++ connect/provider.go
@@ -26,6 +26,16 @@ func Provider() *schema.Provider {
 				Optional:    true,
 				DefaultFunc: schema.EnvDefaultFunc("KAFKA_CONNECT_BASIC_AUTH_PASSWORD", ""),
 			},
+			"tls_auth_crt": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				DefaultFunc: schema.EnvDefaultFunc("KAFKA_CONNECT_TLS_AUTH_CRT", ""),
+			},
+			"tls_auth_key": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				DefaultFunc: schema.EnvDefaultFunc("KAFKA_CONNECT_TLS_AUTH_KEY", ""),
+			},
 		},
 		ConfigureFunc: providerConfigure,
 		ResourcesMap: map[string]*schema.Resource{
@@ -44,6 +54,20 @@ func providerConfigure(d *schema.ResourceData) (interface{}, error) {
 	pass := d.Get("basic_auth_password").(string)
 	if user != "" && pass != "" {
 		c.SetBasicAuth(user, pass)
+	}
+	
+	crt := d.Get("tls_auth_crt").(string)
+	key := d.Get("tls_auth_key").(string)
+	log.Printf("[INFO]Cert : %s\nKey: %s", crt, key)
+
+	if crt != "" && key != "" {
+		cert, err := tls.LoadX509KeyPair(crt, key)
+		if err != nil {
+			log.Fatalf("client: loadkeys: %s", err)
+		} else {
+			c.SetInsecureSSL()
+			c.SetClientCertificates(cert)
+		}
 	}
 	return c, nil
 }
